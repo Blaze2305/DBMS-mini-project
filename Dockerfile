95302c0b0d2528bd37a1253e947f8960befa195e
Pranav <prajpran@gmail.com>
2021-01-15 02:29:03 +0530

    added kubernets yaml file, made changes to dockerfile to run app using gunicorn

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD [ "python","application.py" ]
+CMD ["gunicorn"  , "-w" , "2", "-b", "0.0.0.0:5000", "app:app"]
--- application.py
+++ application.py
@@ -1,7 +1,5 @@
 from app import app as application
-from flask_cors import CORS
 
-CORS(application)
 
 if __name__ == "__main__":
-	application.run(host="0.0.0.0",debug=True)
+	application.run(host="0.0.0.0",debug=False)
